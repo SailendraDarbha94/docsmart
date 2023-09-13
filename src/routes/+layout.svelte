bcce43965434ad462d9308ee7f04ebca4b59034e
Sailendra Darbha <sailendra.darbha@gmail.com>
2023-09-13 15:36:39 +0530

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { AppShell, AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+	import Icon from '@iconify/svelte';
+	import logo from '$lib/assets/light_logo.png'
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<div class="flex justify-center items-center">
+					<img src={logo} class="block rounded-lg w-10 h-10" alt="logo">
+					<strong class="text-xl uppercase">AmpDent</strong>
+				</div>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<!-- <a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://discord.gg/EXqV7W8MtY"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Discord
+				</a> -->
+				<!-- <a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://twitter.com/SkeletonUI"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Twitter
+				</a> -->
+				<LightSwitch />
+				<a
+					class="flex"
+					href="https://github.com/skeletonlabs/skeleton"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<Icon icon="clarity:power-solid" class="h-7 w-7" />
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+
+	<svelte:fragment slot="footer">
+		<div class="flex justify-center bg-red-500">
+			footer
+		</div>
+	</svelte:fragment>
+</AppShell>
